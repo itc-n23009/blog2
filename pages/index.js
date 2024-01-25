297875056ddf65bc0eb15aac10c850013c4fbcf7
itc-n23009 <n23009@std.it-college.ac.jp>
2024-01-25 06:43:46 +0000

    修正点を修正

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,12 @@
-<<<<<<< HEAD
-const Home = () => {
-  return (
-   <>
-      <header>HEADER</header>
-
-      <main>
-        <div>
-          <h1>CUBE</h1>
-          <p>アウトプットしていくサイト</p>
-        </div>
-      <main>
-
-      <footer>FOOTER</footer>
-    </>
-=======
 import Hero from 'components/hero'
 
 const Home = () => {
-<<<<<<< HEAD
   return (
     <Hero
       title='CUBE'
       subtitle='アウトプットしていくサイト'
       imageOn
     />
->>>>>>> chapter2
   )
-=======
-  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn / >
->>>>>>> chapter3
 }
 export default Home
