9b53a15174c21b6e6b3278d616281e731f0852d8
itc-n23009 <n23009@std.it-college.ac.jp>
2024-01-25 06:22:05 +0000

    chapter3 finish

--- components/logo.js
+++ components/logo.js
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import styles from 'styles/logo.module.css'
 
-const Logo = () => {
+const Logo = ({ boxOn = false }) => {
   return (
-    <Link href="/">
-      <a className={boxOn ? styles.box : styles.basic}CUBE</a>
+    <Link href='/' className={boxOn ? styles.box : styles.basic}>
+      CUBE
     </Link>
   )
 }
