346eddffb3fd3436065e77ba03d6f4588e43bd63
itc-n23009 <n23009@std.it-college.ac.jp>
2024-01-25 07:49:51 +0000

    p112~119 finish
    各ページにCssを適用させ、アバウトページに本文を追加させた

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,11 +1,14 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 const Blog = () => {
   return (
+    <Container>
     <Hero
       title='Blog'
       subtitle='Recent Posts'
     />
+    </Container>
   )
 }
 export default Blog
