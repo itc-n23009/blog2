cac8007d2ec03f767ffb058368dcc2b2b04ec9a3
itc-n23009 <n23009@std.it-college.ac.jp>
2024-01-31 05:45:48 +0000

    p191~193 finish
    ページのタイトルの表示を整えた

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
+import Meta from 'components/meta'
 
 const Home = () => {
   return (
     <Container>
+      <Meta />
     <Hero
       title='CUBE'
       subtitle='アウトプットしていくサイト'
