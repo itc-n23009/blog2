e392a7d2fbea5bec852ba4966ebd8c91b2b62df0
itc-n23009 <n23009@std.it-college.ac.jp>
2024-02-08 06:30:49 +0000

    p272~275 finish
    ブログページに記事の一覧を表示させた

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,12 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
+import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Container>
       <Meta pageTitle='ブログ' 
@@ -12,7 +16,26 @@ const Blog = () => {
       title='Blog'
       subtitle='Recent Posts'
     />
+    <Posts posts={posts} />
     </Container>
   )
 }
 export default Blog
+
+export async function getStaticProps () {
+  const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts: posts
+    }
+  }
+}
---
+++ pages/sample2/index.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+   const Sample2 = () => {
+     const [value, setValue] = useState('')
+     const [value1, setValue1] = useState('')
+     const handleChange = e => setValue1(e.target.value)
+     const handleClick = e => setValue(value1) 
+
+     return (
+       <>
+         <p>{value}</p>
+         <inpurt type="text" onChange={handleChange}/>
+         <button onClick={handleClick}>click me</button>
+       </>
+  )
+}
+export default Sample2
